a3132da7d9b856eaae2abc32e9f5037a7fc970b7
sharief007 <shariefmohammad007@gmail.com>
2023-07-27 22:49:51 +0530

    Revert "install vite pwa plugin"
    This reverts commit ec98f46a0f007b3ac700bd9f525282b67e0da4e9.

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
-
-const vuePlugin = vue()
-const pwaPlugin = VitePWA({
-  registerType: 'autoUpdate'
-})
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [vuePlugin, pwaPlugin],
+  base: "/p2pfs/",
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
