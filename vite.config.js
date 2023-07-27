ec98f46a0f007b3ac700bd9f525282b67e0da4e9
sharief007 <shariefmohammad007@gmail.com>
2023-07-27 22:18:01 +0530

    install vite pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+
+const vuePlugin = vue()
+const pwaPlugin = VitePWA({
+  registerType: 'autoUpdate'
+})
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/p2pfs/",
-  plugins: [
-    vue(),
-  ],
+  base: "/",
+  plugins: [vuePlugin, pwaPlugin],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
